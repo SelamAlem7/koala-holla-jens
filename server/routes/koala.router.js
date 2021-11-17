a7fc3c0a08ab0fd654c6ecddba99dff866e83e02
Kelsey Brown <kbrown55347@gmail.com>
2021-11-17 17:15:17 -0600

    created GET route in koala.router file to get koalas table from DB

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -3,9 +3,22 @@ const koalaRouter = express.Router();
 
 // DB CONNECTION
 
+const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', (req, res) => {
+  console.log('GET /koalas');
+  const text = 'SELECT * FROM koalas;';
+  pool.query(text)
+    .then((dbResult) => {
+      console.log(`${dbResult.rows.length} rows to send.`)
+      res.send(dbResult.rows);
+    })
+    .catch((dbErr) => {
+      console.error(dbErr);
+      res.sendStatus(500);
+    });
+});
 
 // POST
 
